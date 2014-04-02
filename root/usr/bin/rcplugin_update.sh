09eb768cdf17fd462b9f722504897c2c2179a46c
Stephane de Labrusse <stephdl@de-labrusse.fr>
2014-04-02 21:38:53 +0200

    updated /usr/bin/rcplugin_update.sh in order to update composer first before to update plugins

--- root/usr/bin/rcplugin_update.sh
+++ root/usr/bin/rcplugin_update.sh
@@ -4,6 +4,7 @@
 /etc/init.d/httpd-e-smith restart >/dev/null 2>&1
 pathroundcube=$(pwd)
 cd /opt/roundcube
+php /opt/roundcube/composer.phar self-update
 php /opt/roundcube/composer.phar update
 /sbin/e-smith/db configuration setprop php AllowUrlFopen off
 /sbin/e-smith/expand-template /etc/php.ini
