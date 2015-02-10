3aa825c69f4a3be5d4089fe18355d6afdbfe4fd6
Stephane de Labrusse <stephdl@de-labrusse.fr>
2015-02-10 18:48:35 +0100

    wip

--- root/usr/bin/rcplugin_update.sh
+++ root/usr/bin/rcplugin_update.sh
@@ -3,9 +3,9 @@
 /sbin/e-smith/expand-template /etc/php.ini
 /etc/init.d/httpd-e-smith restart >/dev/null 2>&1
 pathroundcube=$(pwd)
-cd /opt/roundcube
-php /opt/roundcube/composer.phar self-update
-php /opt/roundcube/composer.phar update
+cd /usr/share/roundcubemail
+php /usr/share/roundcubemail/composer.phar self-update
+php /usr/share/roundcubemail/composer.phar update
 /sbin/e-smith/db configuration setprop php AllowUrlFopen off
 /sbin/e-smith/expand-template /etc/php.ini
 /etc/init.d/httpd-e-smith restart >/dev/null 2>&1
